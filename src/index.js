a3154084efe1ef11a05fe2acf3f8f04bcb363e2a
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-10-22 18:02:07 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const {GraphQLServer} = require('graphql-yoga');
+const { PrismaClient } = require('@prisma/client');
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const { PubSub } = require('graphql-yoga')
+const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
+
+const pubsub = new PubSub()
+
+const prisma = new PrismaClient()
+
+
+const resolvers = {
+    Query,
+    Mutation,
+    Subscription,
+    User,
+    Link,
+    Vote,
+}
+
+const server = new GraphQLServer({
+    typeDefs:'./src/schema.graphql',
+    resolvers,
+    context: request =>{
+        return{
+            ...request,
+            prisma,
+            pubsub
+        }
+    }
+});
+
+server.start(() => console.log('Server is running on http://localhost:4000'));
